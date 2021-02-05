a8e84e656101df5db264eba70c4108fbbab691c2
Artur Caliendo Prado <artur.prado@gmail.com>
2021-02-05 15:16:07 -0300

    Rename ranking attribute

--- ranking/ranking.go
+++ ranking/ranking.go
@@ -1,8 +1,8 @@
 package ranking
 
 type Ranking struct {
-	ID            int    `json:"id"`
-	PlayerName    string `json:"player_name"`
-	Points        int    `json:"points"`
-	CharacterName string `json:"character"`
+	ID         int    `json:"id"`
+	PlayerName string `json:"player_name"`
+	Points     int    `json:"points"`
+	Character  string `json:"character"`
 }
