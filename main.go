cbb92c2d8c8a7f8e4dfb396db7539f081fa29677
Artur Caliendo Prado <artur.prado@gmail.com>
2021-02-05 13:39:29 -0300

    Get http port from Env variables when available

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"star-track.com/star-track-go-api/character"
 )
@@ -10,8 +11,13 @@ import (
 const basePath = "/api"
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
+
 	character.SetupRoutes(basePath)
-	err := http.ListenAndServe(":5000", nil)
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
