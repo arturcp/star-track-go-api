1df1c973309e7ca3f16764eae8531e7a8238d9a7
Artur Caliendo Prado <artur.prado@gmail.com>
2021-02-19 10:38:15 -0300

    Add character history

--- character/character.go
+++ character/character.go
@@ -1,9 +1,10 @@
 package character
 
 type Character struct {
-	ID       int    `json:"id"`
-	Name     string `json:"name"`
-	ImageURL string `json:"image_url"`
-	Bio      string `json:"bio"`
-	Pronouns string `json:"pronouns"`
+	ID       int      `json:"id"`
+	Name     string   `json:"name"`
+	ImageURL string   `json:"image_url"`
+	Bio      string   `json:"bio"`
+	Pronouns string   `json:"pronouns"`
+	History  []string `json:"history"`
 }
