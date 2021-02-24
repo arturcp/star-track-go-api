ffa88938e1e3aa522c321fa9660529bce12e26be
Artur Caliendo Prado <artur.prado@gmail.com>
2021-02-24 16:21:12 -0300

    Expose main color in the API

--- character/character.go
+++ character/character.go
@@ -1,11 +1,12 @@
 package character
 
 type Character struct {
-	ID       int      `json:"id"`
-	Type     string   `json:"type"`
-	Name     string   `json:"name"`
-	ImageURL string   `json:"image_url"`
-	Bio      string   `json:"bio"`
-	Pronouns string   `json:"pronouns"`
-	History  []string `json:"history"`
+	ID        int      `json:"id"`
+	Type      string   `json:"type"`
+	Name      string   `json:"name"`
+	ImageURL  string   `json:"image_url"`
+	Bio       string   `json:"bio"`
+	MainColor string   `json:"main_color"`
+	Pronouns  string   `json:"pronouns"`
+	History   []string `json:"history"`
 }
